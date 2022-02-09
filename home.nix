5a4a33e373c71bac8994a98ed8d98bf13f64f092
autumnal <friedrich122112@me.com>
2022-02-09 17:59:03 +0100

    software

--- home.nix
+++ home.nix
@@ -10,8 +10,5 @@
   home.packages = [
     #pkgs.alacritty
     pkgs.unstable.xterm
-    pkgs.nixfmt
-    pkgs.bitwarden
-    pkgs.neofetch
   ] ++ lib.optional (host == "ft-ssy-sfnb") pkgs.alacritty;
 }
--- modules/desktop/software.nix
+++ modules/desktop/software.nix
@@ -13,5 +13,10 @@
 
     #dev
     rustup
+    nixfmt
+
+    #misc
+    bitwarden
+    neofetch
   ];
 }
