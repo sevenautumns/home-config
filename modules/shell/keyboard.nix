0c8848701195a2bff6230792758d08d54e9e40b9
autumnal <friedrich122112@me.com>
2022-02-28 16:56:59 +0100

    keyboard

--- modules/shell/keyboard.nix
+++ modules/shell/keyboard.nix
@@ -1,39 +1,42 @@
 { pkgs, config, lib, host, ... }:
 let
-  de = {
-    layout = "de";
-    variant = ''""'';
-    model = "pc105";
-    options = [ "caps:ctrl_modifier" "altwin:swap_alt_win" "lv3:rwin_switch" ];
+  apple-options = [ "altwin:swap_alt_win" "lv3:rwin_switch" ];
+  layouts = {
+    de = {
+      layout = "de";
+      variant = ''""'';
+      model = "pc105";
+      options = [ "caps:ctrl_modifier" ];
+    };
+    de_us = {
+      layout = "de";
+      variant = "us";
+      model = "pc105";
+      options = [ "caps:ctrl_modifier" ];
+    };
+    de-apple = { options = layouts.de.options ++ apple-options; } // layouts.de;
+    de_us-apple = {
+      options = layouts.de_us.options ++ apple-options;
+    } // layouts.de_us;
   };
-  de_us = {
-    layout = "de";
-    variant = "us";
-    model = "pc105";
-    options = [ "caps:ctrl_modifier" "altwin:swap_alt_win" "lv3:rwin_switch" ];
-  };
-  make-command = k:
-    (builtins.replaceStrings [ "\n" ] [ " " ] ''
-      ${pkgs.xorg.setxkbmap}/bin/setxkbmap 
-        -layout ${k.layout}
-        -variant ${k.variant}
-        -model ${k.model}
-        ${(builtins.toString (builtins.map (o: "-option " + o) k.options))}
-    '');
 in {
 
   options = with lib; {
-    keyboard-commands = {
-      keyboard-de = mkOption {
+    keyboard-commands = lib.attrsets.mapAttrs' (name: l:
+      lib.attrsets.nameValuePair ("keyboard-" + name) (mkOption {
         type = types.str;
-        default = make-command de;
-      };
-      keyboard-de_us = mkOption {
-        type = types.str;
-        default = make-command de_us;
-      };
-    };
+        default = (builtins.replaceStrings [ "\n" ] [ " " ] ''
+          ${pkgs.xorg.setxkbmap}/bin/setxkbmap 
+            -layout ${l.layout}
+            -variant ${l.variant}
+            -model ${l.model}
+            ${(builtins.toString (builtins.map (o: "-option " + o) l.options))}
+        '');
+      })) layouts;
   };
 
-  config = { home.keyboard = if host == "ft-ssy-sfnb" then de else de_us; };
+  config = with layouts; {
+    home.keyboard =
+      if host == "ft-ssy-sfnb" then layouts.de else layouts.de_us-apple;
+  };
 }
