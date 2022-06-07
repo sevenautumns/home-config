302ff517bac78af3afeff8dd0adf4efea6032286
autumnal <friedrich122112@me.com>
2022-06-07 11:32:21 +0200

    unstable search

--- modules/desktop/software/firefox.nix
+++ modules/desktop/software/firefox.nix
@@ -53,7 +53,7 @@ in {
         "Nix Options" = {
           keyword = ":no";
           url =
-            "https://search.nixos.org/options?channel=21.11&from=0&size=50&sort=relevance&type=packages&query=%s";
+            "https://search.nixos.org/options?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=%s";
         };
         "Nyaa" = {
           keyword = ":nyaa";
