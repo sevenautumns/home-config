39f9d9156bb78dce94505dcc92ef3a3682f24412
Sven Friedrich <sven@autumnal.de>
2023-04-21 09:11:58 +0200

    secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,12 +3,14 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2untVWtTCezJeQxl40TJGsnDvDNXBiUxWnpN4oOdrp";
   frie_sv =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXeNbEgdMSjXN7C22LuaEgj9ppT+zhvyAzYKqiCpn/6";
-  users = [ autumnal frie_sv ];
+  users = [ autumnal frie_sv neesama ];
 
   index =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKd6aKgl2xDyW3/yoWYQa+/sFbiRxXfeXZfOY9vEwTLR";
   castle =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ2tYyAHOAZQs2ME6B6aX5iAxxd9I7jqi1UJW/spYXZK";
+  neesama = 
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE/y3P7h6fiwG95EHTkVFu9rNjr+2J9IIyWlIhFzMPS/";
   systems = [ index castle ];
 in {
   "niketsu.age".publicKeys = [ index ] ++ users;
